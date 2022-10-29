6b0ece511b8a1230cb79da2d441338acb5e859de
k-gn <rlarbska97@gmail.com>
2022-10-29 22:11:33 +0900

    chore: project init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # toppings-server
+
 toppings server
---
+++ src/main/java/com/toppings/ToppingsServerApplication.java
@@ -0,0 +1,13 @@
+package com.toppings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToppingsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToppingsServerApplication.class, args);
+	}
+
+}

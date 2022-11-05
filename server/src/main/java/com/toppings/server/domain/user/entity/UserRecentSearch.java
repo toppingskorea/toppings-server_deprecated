b319649fa513239645a70a084d55469792f55785
k-gn <rlarbska97@gmail.com>
2022-11-05 22:30:16 +0900

    feat: recent entity 추가

---
+++ server/src/main/java/com/toppings/server/domain/user/entity/UserRecentSearch.java
@@ -0,0 +1,41 @@
+package com.toppings.server.domain.user.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import com.toppings.server.domain_global.entity.BaseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "t_recent_search")
+public class UserRecentSearch extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "recent_id")
+	private Long id;
+
+	@Column(name = "recent_keyword")
+	private String keyword;
+}
